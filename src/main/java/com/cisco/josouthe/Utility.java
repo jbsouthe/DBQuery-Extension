d57fda2571655f10069621889da44d32e7193b51
John Southerland <jsoutherland@me.com>
2024-03-13 15:10:48 -0600

    added some utilities

---
+++ src/main/java/com/cisco/josouthe/Utility.java
@@ -0,0 +1,131 @@
+package com.cisco.josouthe;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Utility {
+    private static final Logger logger = LogManager.getFormatterLogger();
+    private static Pattern patternConnectionString = Pattern.compile("^(?<jdbc>[j|J][d|D][b|B][c|C]:)?(?<vendor>[^:]+):(?<driver>[^:]+):(?<path>.*);?");
+    private static Pattern patternAnalyticsDateString = Pattern.compile("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{3}Z");
+    private static SimpleDateFormat analyticsDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+    public static final String controlDateFormatString = "yyy-MM-dd_HH:mm:ss_z";
+    private static SimpleDateFormat controlDateFormat = new SimpleDateFormat(controlDateFormatString);
+
+    public static String encode( String original ){
+        /* this, of course, got rediculous, replacing with a utility external to code base */
+        try {
+            return URLEncoder.encode(original, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            logger.warn("Unsupported Encoding Exception: %s", e.getMessage());
+        }
+        //falling back to the manual, because it is already written..
+        return original.replace("%", "%25")
+                .replace("|","%7C")
+                .replace(" ", "%20")
+                .replace(":","%3A")
+                .replace(".", "%2E")
+                .replace("-", "%2D")
+                .replace("#", "%23")
+                .replace("\"", "%22")
+                .replace("/", "%2F")
+                .replace("(", "%28")
+                .replace(")", "%29")
+                .replace("<", "%3C")
+                .replace(">", "%3E")
+                ;
+    }
+
+    public static String getDateString(long dateTime) { //ISO8601 Date (Extend) https://dencode.com/en/date/iso8601
+        return ZonedDateTime.ofInstant(new Date(dateTime).toInstant(), ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX"));
+    }
+    public static String getEncodedDateString( long dateTime ) {
+        return encode(getDateString(dateTime));
+    }
+
+    public static String escapeQuotes( String input ) {
+        return input.replace("\"", "\\\"");
+    }
+
+    public static String parseDatabaseVendor( String connectionString) {
+        Matcher matcher = patternConnectionString.matcher(connectionString);
+        if(matcher.find()) return matcher.group("vendor");
+        return "oracle";
+    }
+
+    public static String parseDatabasePath( String connectionString ) {
+        Matcher matcher = patternConnectionString.matcher(connectionString);
+        if(matcher.find()) return matcher.group("path");
+        return null;
+    }
+
+    public static String cleanFileName( String in ) {
+        return in.replace('.', '_').replace(' ', '_');
+    }
+
+    public static long now( long adj ) { return now()+adj; }
+    public static long now() { return System.currentTimeMillis(); }
+
+    public static boolean isThisStringADate(String data) {
+        if( data == null ) return false;
+        //"2021-10-14T18:00:51.435Z"
+        Matcher matcher = patternAnalyticsDateString.matcher(data);
+        if(matcher.matches()) return true;
+        return false;
+    }
+
+    public static long parseAnalyticsDateString(String data) throws ParseException {
+        //"2021-10-14T18:00:51.435Z"
+        return analyticsDateFormat.parse(data).getTime();
+    }
+
+    public static long parseControlDateString( String data ) throws ParseException {
+        return controlDateFormat.parse(data).getTime();
+    }
+
+    public static String formatControlDate( long date ) {
+        return formatControlDate( new Date(date) );
+    }
+
+    public static String formatControlDate( Date date ) {
+        return controlDateFormat.format(date);
+    }
+
+    public static boolean isAPIKeyValidFormat(String clientID, String hostname) {
+        if( clientID == null || hostname == null ) return false;
+        if( !clientID.contains("@") ) return false;
+        String apikey = clientID.split("\\@")[0];
+        if( hostname.contains(".") )
+            hostname = hostname.split("\\.")[0];
+        if( String.format("%s@%s",apikey,hostname).equals(clientID) ) return true;
+        return false;
+    }
+
+    public static Level getLevel( String s ) {
+        switch (s.toUpperCase()) {
+            case "TRACE": { return Level.TRACE; }
+            case "DEBUG": { return Level.DEBUG; }
+            case "INFO": { return Level.INFO; }
+            case "WARN": { return Level.WARN; }
+            case "ERROR": { return Level.ERROR; }
+            default: return null;
+        }
+    }
+
+    public static boolean isStringSet( String s ) {
+        if( s == null ) return false;
+        if( "".equalsIgnoreCase(s) ) return false;
+        return true;
+    }
+}
